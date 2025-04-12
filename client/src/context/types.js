f390b83c137fbdf7f4d619e7a7f346cea2f5884a
domnatz <janlnato@su.edu.ph>
2025-04-13 01:00:42 +0800

    types

---
+++ client/src/context/types.js
@@ -0,0 +1,22 @@
+// Assessment Types
+export const GET_ASSESSMENT = 'GET_ASSESSMENT';
+export const SUBMIT_ASSESSMENT = 'SUBMIT_ASSESSMENT';
+export const ASSESSMENT_ERROR = 'ASSESSMENT_ERROR';
+export const CLEAR_ERRORS = 'CLEAR_ERRORS';
+export const SET_LOADING = 'SET_LOADING';
+
+// Task Types
+export const GET_TASKS = 'GET_TASKS';
+export const ADD_TASK = 'ADD_TASK';
+export const UPDATE_TASK = 'UPDATE_TASK';
+export const DELETE_TASK = 'DELETE_TASK';
+export const TASK_ERROR = 'TASK_ERROR';
+
+// Auth Types
+export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
+export const REGISTER_FAIL = 'REGISTER_FAIL';
+export const USER_LOADED = 'USER_LOADED';
+export const AUTH_ERROR = 'AUTH_ERROR';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAIL = 'LOGIN_FAIL';
+export const LOGOUT = 'LOGOUT';
