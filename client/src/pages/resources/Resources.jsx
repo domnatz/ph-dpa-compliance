7148c00ef09653f89cb76230172ee4340ffbea4d
domnatz <janlnato@su.edu.ph>
2025-04-12 21:34:33 +0800

    path fix

--- client/src/pages/resources/Resources.jsx
+++ client/src/pages/resources/Resources.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Helmet } from 'react-helmet-async';
-import api from '../utils/api';
+import api from '../../utils/api';
 
 const Resources = () => {
   const [resources, setResources] = useState([]);
